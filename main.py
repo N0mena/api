7e91a6ee423ae980dd92f78166737c65e36e63e0
N0mena <alida.rajaonarivelo@gmail.com>
2025-07-24 09:26:06 +0300

    Q1-2

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+from starlette.requests import Request
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 def read_root():
-    return {"message": "Bienvenue sur le test"}
+    return JSONResponse({"message": f"Hello World"}, status_code=200)
+
+
+class WelcomeRequest(BaseModel):
+    name: str
+
+@app.get("/welcome")
+def welcome_user(request: Request, name :str ):
+    return JSONResponse({"message": f"Welcome {name}"}, status_code=200)
+
+
+class StudentsRequest(BaseModel):
+    Reference: str
+    FirstName: str
+    LastName: str
+    Age: int
+
+
+@app.post("/students")
+def students(request : StudentsRequest):
+    return JSONResponse(
+        content={[request]},
+        status_code=300)
+
+
+
+@app.get("/students")
+def students():
+    return Response()
